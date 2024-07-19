9230a08d7e005e7bf64ec9b03fe707dfe326629c
chun <kochun335@gmail.com>
2024-07-19 18:20:48 +0100

    - Add XPATH to close "Save login window" - Add the name of a celebrity account - Add Username and pw

---
+++ main.py
@@ -0,0 +1,4 @@
+SAVE_LOGIN_INFO_XPATH = "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/section/div/button"
+SIMILAR_ACCOUNT = "officialallmanbrothers"
+USERNAME = "dmc.dante.zzzz"
+PW = "Kochun1!"
